2b3d194e4e4f79b8495bfe34be9c2527866b7dd5
helypereira <almeidahely@gmail.com>
2024-11-29 23:04:51 -0300

    3.1 backend phonebook step 1

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import people from './contact.js';
+
+const app = express();
+const port = 3001;
+
+app.get("/api/personas", (req, res) => {
+    res.send(people);
+});
+
+app.get("/info", (req, res) => {
+    const ids = people.map(person => person.id);
+    const requestTime = new Date();
+    res.send(`Phonebook has info for ${ids.length} people <br/>
+        ${requestTime}`);
+});
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`)); 
+
+
