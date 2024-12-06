8690222ab8555365bd59952ea0cb7c29ddcbface
helypereira <almeidahely@gmail.com>
2024-12-06 17:23:04 -0300

    3.3 phonebook step 3

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ import people from './contact.js';
 const app = express();
 const port = 3001;
 
-app.get("/api/personas", (req, res) => {
-    res.send(people);
+app.get("/api/persons", (req, res) => {
+    res.json(people);
 });
 
-// step 2
 app.get("/info", (req, res) => {
     const ids = people.map(person => person.id);
     const requestTime = new Date();
     res.send(`Phonebook has info for ${ids.length} people <br/>
         ${requestTime}`);
 });
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const person = people.find(person => person.id === id);
+    person ? res.send(person) : res.status(404).send({message: 'person no found'})
+})
+
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`)); 
 
