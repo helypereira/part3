406c0392de1a17bf0f277c39fd9f37757393afdc
Hely Almeida <helyalmeida@MacBookAir.lan>
2025-08-21 20:27:38 -0400

    Add dist

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json());
-
+app.use(express.static('dist'))
 
 const PORT = process.env.PORT || 3001
 
